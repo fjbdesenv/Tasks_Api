1711611d2b0150ab4ab66f20ef333843db87479a
Fábio Júnior Barbosa <fjb.desenv@gmail.com>
2023-11-23 06:41:02 -0300

    Corrreção de importação

--- src/doc/swagger.js
+++ src/doc/swagger.js
@@ -1,5 +1,5 @@
-const swaggerUi = require("swagger-ui-express");
-const swaggerFile =  require("./swagger.json");
+import swaggerUi from "swagger-ui-express";
+import swaggerFile from "./swagger.json";
 
 module.exports = { swaggerUi, swaggerFile };
 
